466b6d91fbcd3e43eb4fbbfa6118935002196245
Vineeth <beeramvineethreddy123@gmail.com>
2020-05-19 00:20:59 +0530

    Components Part-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-
+/*this import allows us to define a component decorator
+ and apply to a class
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  /*
+      app-root can be applied in the index.html page as a tag
+      <app-root></app-root>
+  */
 })
 export class AppComponent {
   title = 'conFusion';
