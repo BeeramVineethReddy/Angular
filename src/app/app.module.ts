482ad47ab6794a625db0f718b77a6acd4caa01cf
Vineeth <beeramvineethreddy123@gmail.com>
2020-06-02 00:43:28 +0530

    promise part-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AppComponent } from './app.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -58,6 +59,7 @@ import { LoginComponent } from './login/login.component';
     MatButtonModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
     AppRoutingModule,
     MatDialogModule,
     FormsModule,
